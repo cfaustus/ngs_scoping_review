825f6e14cfd06929ca5efed8a8274b482b6a6db2
cfaustus <christina.faust@gmail.com>
2023-07-27 11:57:08 +0100

    check some of the sample sizes

--- exploratory_analysis.R
+++ exploratory_analysis.R
@@ -20,7 +20,10 @@ str(db4)
 
 sample_cols = grep('sample_size',names(db4)) # identifying sample size columns
 db4$total_samples = rowSums(db4[,sample_cols], na.rm=TRUE)
-db4$total_domains = rowSums(db4[,sample_cols], na.rm=TRUE)
+db4$env_bin = ifelse(db4$env_domain>0,1,0)
+db4$ani_bin = ifelse(db4$ani_sample_size>0,1,0)
+db4$human_bin = ifelse(db4$hum_sample_size>0,1,0)
+db4$total_domains = rowSums(db4[,c('env_bin','ani_bin','human_bin')], na.rm=T)
 # some are 0?!? and some are 1?
 
 # by publication year
@@ -50,6 +53,18 @@ ggplot(db4, aes(x=publication_year, fill=study_aim_1)) +
   scale_y_continuous(expand = c(0, 0), breaks=seq(0,23,2))+
   scale_x_continuous(breaks=seq(2011,2022,1))
 
-# by publication year
+# by sample size year
+names(db4)
+ggplot(db4, aes(x=total_samples, y = total_domains, col=study_aim_1)) +
+  geom_point()+
+  theme(legend.position="top")+
+  theme_classic()+
+  labs(x="total samples", y = "domains")+
+  scale_x_continuous(trans='log10')
 
-
+ggplot(db4, aes(x=total_samples, y = total_domains, col=agent_type)) +
+  geom_point()+
+  theme(legend.position="top")+
+  theme_classic()+
+  labs(x="total samples", y = "domains")+
+  scale_x_continuous(trans='log10')
