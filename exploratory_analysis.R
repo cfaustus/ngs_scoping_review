65b397b8dcd5d6b4084a71695679d61487cd35cb
cfaustus <christina.faust@gmail.com>
2023-09-13 20:09:40 +0100

    database

--- exploratory_analysis.R
+++ exploratory_analysis.R
@@ -6,40 +6,50 @@ library(ggalluvial)
 library(dplyr)
 
 # importing and correctly formatting data
-db4 <- read.csv("data/NGSscoping_dbv4.csv", header = TRUE)
-names(db4)<-tolower(names(db4))
-db4$biotic.env_sample_size
-colnames(db4)[22] <- 'biotic_env_sample_size'
+db <- read.csv("data/NGSscoping_dbv6.csv", header = TRUE)
+names(db)<-tolower(names(db))
+db$biotic.env_sample_size
+colnames(db)[29] <- 'biotic_env_sample_size'
 
 #'env_domain' looks like a sample size.... 
 
-str(db4) # lots of columns coming up as characters
+str(db) # lots of columns coming up as characters
 col.chr.ids = c('hum_sample_size','livestock_sample_size', 'pet_sample_size','poultry_sample_size',
                 'wildlife_sample_size','arachnida_sample_size', 'insecta_sample_size', 'env_domain',
                 'biotic_env_sample_size','abiotic_sample_size')
-db4[col.chr.ids] <- sapply(db4[col.chr.ids],as.numeric)
-str(db4)
+db[col.chr.ids] <- sapply(db[col.chr.ids],as.numeric)
+str(db)
 
-#sample_cols = grep('sample_size',names(db4)) # identifying sample size columns - WRONG double counting
-db4$total_samples = rowSums(db4[,c('hum_sample_size','ani_sample_size', 'env_domain')], na.rm=TRUE)
-db4$env_bin = ifelse(db4$env_domain>0,1,0)
-db4$ani_bin = ifelse(db4$ani_sample_size>0,1,0)
-db4$human_bin = ifelse(db4$hum_sample_size>0,1,0)
-db4$total_domains = rowSums(db4[,c('env_bin','ani_bin','human_bin')], na.rm=T)
+#sample_cols = grep('sample_size',names(db)) # identifying sample size columns - WRONG double counting
+db$total_samples = rowSums(db[,c('hum_sample_size','ani_sample_size', 'env_domain')], na.rm=TRUE)
+db$env_bin = ifelse(db$env_domain>0,1,0)
+db$ani_bin = ifelse(db$ani_sample_size>0,1,0)
+db$human_bin = ifelse(db$hum_sample_size>0,1,0)
+db$total_domains = rowSums(db[,c('env_bin','ani_bin','human_bin')], na.rm=T)
 
 # some are 0?!? and some are 1?
 # annotation in excel clarified there were 276 samples from human and environment but split not given
-db4$total_domains[19]<-2
-db4$total_samples[19]<-276
-db4$env_bin[19] = 1
-db4$human_bin[19] = 1
+
+ind = which(db$total_samples == 0)
+db$total_domains[ind]<-2
+db$total_samples[ind]<-276
+db$env_bin[ind] = 1
+db$human_bin[ind] = 1
 
 
 
 # by publication year
-sort(unique(db4$publication_year))
+sort(unique(db$publication_year))
 library(ggplot2)
-ggplot(db4, aes(x=publication_year, fill=agent_type)) +
+ggplot(db, aes(x=publication_year, fill=agent_type)) +
+  geom_histogram(col = 'white', position="stack", bins = 12)+
+  theme(legend.position="top")+
+  theme_classic()+
+  labs(x="year published", y = "publications")+
+  scale_y_continuous(expand = c(0, 0), breaks=seq(0,23,2))+
+  scale_x_continuous(breaks=seq(2011,2022,1))
+db$ngs_platform_primary
+ggplot(db, aes(x=publication_year, fill=ngs_platform_primary)) +
   geom_histogram(col = 'white', position="stack", bins = 12)+
   theme(legend.position="top")+
   theme_classic()+
@@ -47,15 +57,7 @@ ggplot(db4, aes(x=publication_year, fill=agent_type)) +
   scale_y_continuous(expand = c(0, 0), breaks=seq(0,23,2))+
   scale_x_continuous(breaks=seq(2011,2022,1))
 
-ggplot(db4, aes(x=publication_year, fill=ngs_platform_short_primary)) +
-  geom_histogram(col = 'white', position="stack", bins = 12)+
-  theme(legend.position="top")+
-  theme_classic()+
-  labs(x="year published", y = "publications")+
-  scale_y_continuous(expand = c(0, 0), breaks=seq(0,23,2))+
-  scale_x_continuous(breaks=seq(2011,2022,1))
-
-ggplot(db4, aes(x=publication_year, fill=study_aim_1)) +
+ggplot(db, aes(x=publication_year, fill=study_aim_1)) +
   geom_histogram(col = 'white', position="stack", bins = 12)+
   theme(legend.position="top")+
   theme_classic()+
@@ -64,15 +66,15 @@ ggplot(db4, aes(x=publication_year, fill=study_aim_1)) +
   scale_x_continuous(breaks=seq(2011,2022,1))
 
 # by sample size year
-names(db4)
-ggplot(db4, aes(x=total_samples, y = total_domains, col=study_aim_1)) +
+names(db)
+ggplot(db, aes(x=total_samples, y = total_domains, col=study_aim_1)) +
   geom_point()+
   theme(legend.position="top")+
   theme_classic()+
   labs(x="total samples", y = "domains")+
   scale_x_continuous(trans='log10')
 
-ggplot(db4, aes(x=total_samples, y = total_domains, col=agent_type)) +
+ggplot(db, aes(x=total_samples, y = total_domains, col=agent_type)) +
   geom_point()+
   theme(legend.position="top")+
   theme_classic()+
@@ -80,9 +82,9 @@ ggplot(db4, aes(x=total_samples, y = total_domains, col=agent_type)) +
   scale_x_continuous(trans='log10')
 
 # https://cran.r-project.org/web/packages/ggalluvial/vignettes/ggalluvial.html
-head(db4)
-db4_allu = is_alluvia_form(as.data.frame(db4), axes = 1:3, silent = TRUE)
-ggplot(data = db4,
+head(db)
+db_allu = is_alluvia_form(as.data.frame(db), axes = 1:3, silent = TRUE)
+ggplot(data = db,
        aes(x = publication_year, y = total_samples, alluvium = study_aim_1)) +
   geom_alluvium(aes(fill = study_aim_1, colour = study_aim_1),
                 alpha = .75, decreasing = FALSE) 
@@ -95,35 +97,35 @@ ggplot(data = db4,
   ggtitle("refugee volume by country and region of origin")
   
 
-db4$ngs_platform_short_primary
-head(db4)
-db4_summary = as.data.frame(db4 %>% group_by(ngs_platform_short_primary, publication_year, ngs_platform_company) %>%
+db$ngs_platform_short_primary
+head(db)
+db_summary = as.data.frame(db %>% group_by(ngs_platform_primary, publication_year) %>%
                                 summarise(papers = n()))
-db4_summary$ngs_platform_short_primary=as.factor(db4_summary$ngs_platform_short_primary)
+db_summary$ngs_platform_primary=as.factor(db_summary$ngs_platform_primary)
 
-db4_summary = db4_summary[1:45,]
-db4_summary$ngs_platform_short_primary
-unique(db4$ngs_platform_long)
+db_summary = db_summary[1:45,]
+db_summary$ngs_platform_short_primary
+unique(db$ngs_platform_primary)
 
-db4_summary$ngs_platform_short_primary = as.factor(db4_summary$ngs_platform_short_primary)
+db_summary$ngs_platform_primary = as.factor(db_summary$ngs_platform_primary)
 
-levels(db4_summary$ngs_platform_short_primary)
-db4_summary$ngs_platform_short_primary <- factor(db4_summary$ngs_platform_short_primary, 
-                levels = c("Illumina_NovaSeq","BGI_MGISEQ2000", 
-                           "Illumina_iSeq100","BGI_BGISEQ50", 
-                           "BGI_BGISEQ500", "Illumina_NextSeq", 
+levels(db_summary$ngs_platform_primary)
+db_summary$ngs_platform_primary <- factor(db_summary$ngs_platform_primary, 
+                levels = c("BGI_MGISEQ2000", "Illumina_iSeq100","Illumina_NovaSeq",
+                           "BGI_BGISEQ50", 
+                           "BGI_BGISEQ500", "ONT_MinION","Illumina_NextSeq", 
                            "Illumina_MiSeq", "IonTorrent_IonPGM", 
                            "Illumina_HiSeq", "Illumina_GAIIx", 
                             "Roche_454"))
 
-ggplot(db4_summary, aes(x = publication_year, y = ngs_platform_short_primary )) +
+ggplot(db_summary, aes(x = publication_year, y = ngs_platform_short_primary )) +
   geom_point(aes(size = papers, color = ngs_platform_company))+ # aes(dotsize = papers),
   scale_x_continuous(breaks=seq(2011,2022,1))+
   theme_classic()+
   labs(x="publication year", y = "sequencer")+
-  scale_size_continuous(breaks = c(min(db4_summary$papers),
+  scale_size_continuous(breaks = c(min(db_summary$papers),
                                     3,
-                                    max(db4_summary$papers)),
+                                    max(db_summary$papers)),
                          labels = c("1",
                                     "3",
                                     "9"))+
@@ -139,15 +141,15 @@ ggplot(db4_summary, aes(x = publication_year, y = ngs_platform_short_primary )) 
 # open software
 # open code
 # colored by country?
-db4$public_data_binary = !db4$public_data == 'N/A'
-db4$public_code_binary = db4$public_code == 'yes'
-db4$phylo_software_binary = !db4$phylo_software == 'Geneious'
-db4$sampling_geo
+db$public_data_binary = !db$public_data == 'N/A'
+db$public_code_binary = db$public_code == 'yes'
+db$phylo_software_binary = !db$phylo_software == 'Geneious'
+db$sampling_geo
 
-db4_summary2 = as.data.frame(db4 %>% group_by(public_data_binary, public_code_binary, phylo_software_binary,sampling_geo) %>%
+db_summary2 = as.data.frame(db %>% group_by(public_data_binary, public_code_binary, phylo_software_binary,sampling_geo) %>%
                               summarise(Freq = n()))
 
-ggplot(data = db4_summary2,
+ggplot(data = db_summary2,
        aes(axis1 = as.factor(phylo_software_binary), axis2 = public_data_binary,axis3 = public_code_binary, 
            y = Freq)) +
   scale_x_discrete(limits = c("software", "data", "code"), expand = c(.2, .05)) +
