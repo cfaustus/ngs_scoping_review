65ec95099e169ebc666b5edd3d78fd65736c6b5d
cfaustus <christina.faust@gmail.com>
2023-11-26 17:35:42 +0000

    fixing colors and sequencing figure

--- figures_for_publication.R
+++ figures_for_publication.R
@@ -3,18 +3,20 @@
 
 # color pallettes
 # agent types 8 categories (5 discrete - 3 virus, 2 bacteria)
-# c(
-'#D1B490','#EE7B30','#CBCBD4',
-'#56766E','#ACC3BD','#2B3B37'
-'#885A89','#3D283E')
+# # c(
+# '#D1B490','#EE7B30','#CBCBD4',
+# '#56766E','#ACC3BD','#2B3B37'
+# '#885A89','#3D283E')
 # study aim 5 categories (discrete) < jaynas
 # 6 colours for sequencers
-wes anderson Zissou
+# wes anderson Zissou
 
 # libraries
 library(ggplot2)
 library(ggalluvial)
 library(dplyr)
+library(plyr)
+library(wesanderson)
 
 # importing and correctly formatting data
 db <- read.csv("data/NGSscoping_dbv6.csv", header = TRUE)
@@ -48,19 +50,23 @@ db$env_bin[ind] = 1
 db$human_bin[ind] = 1
 
 
-
-
-# by publication year
+# temporal trends
 sort(unique(db$publication_year))
-library(ggplot2)
+# summarizing agent type into brader categories
+db = db |> mutate(agent_type = ifelse(str_detect(agent_type, "bacterium") == TRUE, "bacterium", agent_type)) |>
+  mutate(agent_type = ifelse(str_detect(agent_type, "virus") == TRUE, "virus", agent_type))
+
+#  figure 1 - annual trend by pathogen type
 ggplot(db, aes(x=publication_year, fill=agent_type)) +
   geom_histogram(col = 'white', position="stack", bins = 12)+
   theme(legend.position="top")+
+  scale_fill_ordinal()+
   theme_classic()+
   labs(x="year published", y = "publications")+
   scale_y_continuous(expand = c(0, 0), breaks=seq(0,23,2))+
   scale_x_continuous(breaks=seq(2011,2022,1))
-db$ngs_platform_primary
+
+# extra figure - annual trend by NGS sequencer
 ggplot(db, aes(x=publication_year, fill=ngs_platform_primary)) +
   geom_histogram(col = 'white', position="stack", bins = 12)+
   theme(legend.position="top")+
@@ -69,90 +75,67 @@ ggplot(db, aes(x=publication_year, fill=ngs_platform_primary)) +
   scale_y_continuous(expand = c(0, 0), breaks=seq(0,23,2))+
   scale_x_continuous(breaks=seq(2011,2022,1))
 
+# extra figure - annual trend by study aim - in SI
 ggplot(db, aes(x=publication_year, fill=study_aim_1)) +
   geom_histogram(col = 'white', position="stack", bins = 12)+
   theme(legend.position="top")+
+  facet_wrap(~study_aim_1, ncol = 1)+
   theme_classic()+
+  theme(legend.position = "none")+
   labs(x="year published", y = "publications")+
   scale_y_continuous(expand = c(0, 0), breaks=seq(0,23,2))+
   scale_x_continuous(breaks=seq(2011,2022,1))
 
-# by sample size year
-names(db)
-ggplot(db, aes(x=total_samples, y = total_domains, col=study_aim_1)) +
-  geom_point()+
-  theme(legend.position="top")+
-  theme_classic()+
-  labs(x="total samples", y = "domains")+
-  scale_x_continuous(trans='log10')
 
-ggplot(db, aes(x=total_samples, y = total_domains, col=agent_type)) +
-  geom_point()+
-  theme(legend.position="top")+
-  theme_classic()+
-  labs(x="total samples", y = "domains")+
-  scale_x_continuous(trans='log10')
-
-# https://cran.r-project.org/web/packages/ggalluvial/vignettes/ggalluvial.html
+# NGS Sequencers over time
+# NGS company
+read = 
 head(db)
-db_allu = is_alluvia_form(as.data.frame(db), axes = 1:3, silent = TRUE)
-ggplot(data = db,
-       aes(x = publication_year, y = total_samples, alluvium = study_aim_1)) +
-  geom_alluvium(aes(fill = study_aim_1, colour = study_aim_1),
-                alpha = .75, decreasing = FALSE) 
-scale_x_continuous(breaks = seq(2003, 2013, 2)) +
-  theme_bw() +
-  theme(axis.text.x = element_text(angle = -30, hjust = 0)) +
-  scale_fill_brewer(type = "qual", palette = "Set3") +
-  scale_color_brewer(type = "qual", palette = "Set3") +
-  facet_wrap(~ region, scales = "fixed") +
-  ggtitle("refugee volume by country and region of origin")
+
+db_platform = as.data.frame(db %>% group_by(ngs_platform_primary, publication_year) %>%
+                             dplyr::summarise(papers = n()))
+db_platform$ngs_platform_primary=as.factor(db_platform$ngs_platform_primary)
+missing = grep('N/S', db_platform$ngs_platform_primary)
+db_platform= db_platform %>%  filter(!row_number() %in% missing)
+
+seq = read.csv('data/sequencing_tech_v2.csv', header = TRUE)
+names(seq)
+seq_trun = seq[,c('company', 'sequence','type')]
+db_platform <- merge(x=db_platform,y=seq_trun, by.x=c("ngs_platform_primary"), 
+                     by.y=c("sequence"), all = TRUE)
+
+db_platform$type = as.factor(db_platform$type)
+db_platform$type <- factor(db_platform$type, 
+                                           levels = c("MGISEQ2000", "iSeq100","NovaSeq",
+                                                      "BGISEQ50", 
+                                                      "BGISEQ500", "MinION","NextSeq",'RSII',
+                                                      "MiSeq", "IonPGM", 
+                                                      "HiSeq", "GAIIx", 
+                                                      "454"))
 
 
-db$ngs_platform_short_primary
-head(db)
-db_summary = as.data.frame(db %>% group_by(ngs_platform_primary, publication_year) %>%
-                             summarise(papers = n()))
-db_summary$ngs_platform_primary=as.factor(db_summary$ngs_platform_primary)
+seq3 = read.csv('data/sequencing_tech_v3.csv', header = TRUE)
 
-db_summary = db_summary[1:45,]
-db_summary$ngs_platform_short_primary
-unique(db$ngs_platform_primary)
+seq_col = wes_palette('Darjeeling1', 6, type = c("continuous"))
 
-db_summary$ngs_platform_primary = as.factor(db_summary$ngs_platform_primary)
-
-levels(db_summary$ngs_platform_primary)
-db_summary$ngs_platform_primary <- factor(db_summary$ngs_platform_primary, 
-                                          levels = c("BGI_MGISEQ2000", "Illumina_iSeq100","Illumina_NovaSeq",
-                                                     "BGI_BGISEQ50", 
-                                                     "BGI_BGISEQ500", "ONT_MinION","Illumina_NextSeq", 
-                                                     "Illumina_MiSeq", "IonTorrent_IonPGM", 
-                                                     "Illumina_HiSeq", "Illumina_GAIIx", 
-                                                     "Roche_454"))
-
-ggplot(db_summary, aes(x = publication_year, y = ngs_platform_short_primary )) +
-  geom_point(aes(size = papers, color = ngs_platform_company))+ # aes(dotsize = papers),
-  scale_x_continuous(breaks=seq(2011,2022,1))+
+ggplot(db_platform, aes(x = publication_year, y = type)) +
+  geom_point(aes(size = papers, color = company))+ # aes(dotsize = papers),
+  scale_x_continuous(breaks=seq(2006,2022,1))+
   theme_classic()+
   labs(x="publication year", y = "sequencer")+
+  scale_color_manual(values = seq_col)+
+  geom_line(data = seq3, aes(y = type,x = publication_year, color = company))+
   scale_size_continuous(breaks = c(min(db_summary$papers),
                                    3,
                                    max(db_summary$papers)),
                         labels = c("1",
                                    "3",
-                                   "9"))+
-  geom_rect(aes(xmin = 2011, xmax = 2022, ymin = "Illumina_HiSeq", ymax = "Illumina_HiSeq"))+
-  annotate("rect", x = 2011, y = 2022, ymin = 12, ymax = 28)
-# need to combine short read and long read sequencing
+                                   "9"))
 
-# is within host diversity studies predicted by mutation rate?
-# maybe not relevant b/c no host diversity
+
 
 # alluvial plot to make
-# open data 
-# open software
-# open code
-# colored by country?
+
 db$public_data_binary = !db$public_data == 'N/A'
 db$public_code_binary = db$public_code == 'yes'
 db$phylo_software_binary = !db$phylo_software == 'Geneious'
@@ -174,21 +157,20 @@ ggplot(data = db_summary2,
 # figure - sample size by HG status?
 # software updates? license 
 
-db5 = read.csv('data/NGSscoping_dbv5.csv')
-names(db5)<-tolower(names(db5))
-db5$geo_scope = ifelse(db5$lic_lmic == 'yes' & db5$umic_hic == 'yes', 'both',
-                       ifelse(db5$lic_lmic == 'yes', 'lmic', 'hic'))
 
-db5$public_code
-db5$public_data_binary = !db5$public_data == 'N/A'
-db5$public_code_binary = db5$public_code == 'yes'
-db5$phylo_software_binary = db5$assembly.mapping_license == 'free'
-db5_summary2 = as.data.frame(db5 %>% group_by(public_data_binary, public_code_binary, phylo_software_binary,geo_scope) %>%
+db$geo_scope = ifelse(db$lic_lmic == 'yes' & db$umic_hic == 'yes', 'both',
+                       ifelse(db$lic_lmic == 'yes', 'lmic', 'hic'))
+
+db$public_code
+db$public_data_binary = !db$public_data == 'N/A'
+db$public_code_binary = db$public_code == 'yes'
+db$phylo_software_binary = db$assembly.mapping_license == 'free'
+db_summary2 = as.data.frame(db %>% group_by(public_data_binary, public_code_binary, phylo_software_binary,geo_scope) %>%
                                summarise(Freq = n()))
 
-db5_summary2$geo_scope = as.factor(db5_summary2$geo_scope)
-db5_summary2$geo_scope = factor(db5_summary2$geo_scope, levels = c('lmic','both','hic'))
-ggplot(data = db5_summary2,
+db_summary2$geo_scope = as.factor(db_summary2$geo_scope)
+db_summary2$geo_scope = factor(db_summary2$geo_scope, levels = c('lmic','both','hic'))
+ggplot(data = db_summary2,
        aes(axis1 = phylo_software_binary, axis2 = public_data_binary,axis3 = public_code_binary, 
            y = Freq)) +
   scale_x_discrete(limits = c("software", "data", "code"), expand = c(.2, .05)) +
@@ -202,13 +184,25 @@ ggplot(data = db5_summary2,
 
 
 
-names(db5)
-db5$time_to_pub = db5$publication_year-as.numeric(db5$last_sample_date)
-ggplot(db5, aes(x=time_to_pub, fill=study_aim_1)) +
-  geom_density(alpha = 0.4)+
+names(db)
+db$time_to_pub = db$publication_year-as.numeric(db$last_sample_date)
+aim_col = wes_palette('AsteroidCity3', 5, type = c("continuous"))
+aim_col
+db_me <- ddply(db, .(study_aim_1), numcolwise(median, na.rm = TRUE))
+db_mean <- ddply(db, .(study_aim_1), numcolwise(mean, na.rm = TRUE))
+
+ggplot(db, aes(x=time_to_pub, fill=study_aim_1)) +
+  geom_density(alpha = 0.80)+
   theme(legend.position="top")+
-  # facet_wrap(~study_aim_1, nrows = 5) +
+  facet_wrap(~study_aim_1, nrow= 5) +
+  scale_fill_manual(values = aim_col)+
   theme_classic()+
+  geom_vline(data=db_me, aes(xintercept=time_to_pub, colour=study_aim_1),
+             linetype="solid", size=0.5) +
+  geom_vline(data=db_mean, aes(xintercept=time_to_pub, colour=study_aim_1),
+             linetype="dashed", size=0.5) +
+  theme(legend.position = "none")+
+  scale_color_manual(values = aim_col)+
   labs(x="years from last collection to publication", y = "papers")
 
 
